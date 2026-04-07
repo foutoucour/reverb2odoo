a0df59bc6003f54d11c8abfdbfdabcb5a8046fc8
Jordi Riera <jordi.riera@autodesk.com>
2026-04-07 09:10:40 -0400

    field
    Signed-off-by: Jordi Riera <jordi.riera@autodesk.com>

--- set_default_currency.py
+++ set_default_currency.py
@@ -1,8 +1,9 @@
-"""Set CAD as the default currency for x_studio_currency_id on a given model.
+"""Set CAD as the default currency for a currency field on a given model.
 
 Usage::
 
     reverb2odoo set-default-currency x_gear
+    reverb2odoo set-default-currency x_gear --field x_currency_id
 """
 
 from __future__ import annotations
@@ -11,13 +12,19 @@ import click
 from loguru import logger
 
 _CURRENCY_NAME = "CAD"
-_FIELD_NAME = "x_studio_currency_id"
+_DEFAULT_FIELD = "x_studio_currency_id"
 
 
 @click.command("set-default-currency")
 @click.argument("model")
+@click.option(
+    "--field",
+    default=_DEFAULT_FIELD,
+    show_default=True,
+    help="Currency field name to set the default on.",
+)
 @click.pass_context
-def cli(ctx: click.Context, model: str) -> None:
+def cli(ctx: click.Context, model: str, field: str) -> None:
     """Set CAD as the default currency for MODEL.
 
     MODEL is the Odoo model name (e.g. x_gear).
@@ -30,10 +37,10 @@ def cli(ctx: click.Context, model: str) -> None:
         raise SystemExit(1)
     cad_id = cad_ids[0]
 
-    conn.get_model("ir.default").set(model, _FIELD_NAME, cad_id)
+    conn.get_model("ir.default").set(model, field, cad_id)
     logger.success(
         "Default {} set to {} (id={}) on model '{}'.",
-        _FIELD_NAME,
+        field,
         _CURRENCY_NAME,
         cad_id,
         model,
